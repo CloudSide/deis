ee0ed83ac6ba1e48e38ed6954d6431d5ea5af204
科学家 <smcz@qq.com>
2016-01-21 14:19:21 +0800

    Revert "Update includes.mk"
    This reverts commit 4e86170b310897b3e325aca2814a0749db937d4c.

--- includes.mk
+++ includes.mk
@@ -57,7 +57,7 @@ check-deisctl:
 	fi
 
 define check-static-binary
-  @if file $(1) | egrep -q "(statically linked|Mach-O)"; then \
+  if file $(1) | egrep -q "(statically linked|Mach-O)"; then \
     echo -n ""; \
   else \
     echo "The binary file $(1) is not statically linked. Build canceled"; \
